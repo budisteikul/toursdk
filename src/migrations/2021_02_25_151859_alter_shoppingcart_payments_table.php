60e55277538458c992767bf3478e90db1b67a8e2
Budi <aku@budi.my.id>
2021-02-27 16:46:27 +0700

    Update 2021_02_25_151859_alter_shoppingcart_payments_table.php

--- src/migrations/2021_02_25_151859_alter_shoppingcart_payments_table.php
+++ src/migrations/2021_02_25_151859_alter_shoppingcart_payments_table.php
@@ -17,6 +17,9 @@ class AlterShoppingcartPaymentsTable extends Migration
             $table->float('rate')->after('currency')->default(0);
             $table->string('rate_from')->after('rate')->nullable();
             $table->string('rate_to')->after('rate_from')->nullable();
+            $table->string('type')->after('rate_to')->nullable();
+            $table->string('method')->after('type')->default('full_payment');
+            $table->float('deposit')->after('method')->default(0);
         });
     }
 
@@ -31,6 +34,9 @@ class AlterShoppingcartPaymentsTable extends Migration
              $table->dropColumn('rate');
              $table->dropColumn('rate_from');
              $table->dropColumn('rate_to');
+             $table->dropColumn('type');
+             $table->dropColumn('method');
+             $table->dropColumn('deposit');
         });
     }
 }

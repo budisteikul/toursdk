f74122199ffee56f409feb6dc23468f400171f82
Budi <aku@budi.my.id>
2022-03-18 00:37:24 +0700

    asd

---
+++ src/Libs/SimpleSoftwareIO/QrCode/Image.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SimpleSoftwareIO\QrCode;
+
+class Image implements ImageInterface
+{
+    /**
+     * Holds the image resource.
+     *
+     * @var resource
+     */
+    protected $image;
+
+    /**
+     * Creates a new Image object.
+     *
+     * @param $image string An image string
+     */
+    public function __construct($image)
+    {
+        $image = imagecreatefromstring($image);
+        imageinterlace($image, false);
+        $this->image = $image;
+    }
+
+    /*
+     * Returns the width of an image
+     *
+     * @return int
+    */
+    public function getWidth()
+    {
+        return imagesx($this->image);
+    }
+
+    /*
+     * Returns the height of an image
+     *
+     * @return int
+     */
+    public function getHeight()
+    {
+        return imagesy($this->image);
+    }
+
+    /**
+     * Returns the image string.
+     *
+     * @return string
+     */
+    public function getImageResource()
+    {
+        return $this->image;
+    }
+}

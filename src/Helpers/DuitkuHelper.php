2757b02ae403e1384b12e6b9167aa3787ce688ef
Budi <aku@budi.my.id>
2022-06-05 10:25:01 +0700

    Update DuitkuHelper.php

--- src/Helpers/DuitkuHelper.php
+++ src/Helpers/DuitkuHelper.php
@@ -131,13 +131,13 @@ class DuitkuHelper {
         }
         else if($payment->bank_payment_type=="LA")
         {
-            $data1 = self::createTransaction($data,$payment);
+            //$data1 = self::createTransaction($data,$payment);
 
-            //$data1 = self::createSnap($data);
-            //$data2 = self::createCharge($data1->reference,$payment);
+            $data1 = self::createSnap($data);
+            $data2 = self::createCharge($data1->reference,$payment);
             //$data2 = self::createCharge($data1->reference,$payment);
             print_r($data1);
-            //print_r($data2);
+            print_r($data2);
             exit();
 
             $response->payment_type = 'ewallet';

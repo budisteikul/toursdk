c3e5f29391a3fb04ce23018b89a44ee712e123f5
Budi <aku@budi.my.id>
2022-06-06 19:39:39 +0700

    Update DanaHelper.php

--- src/Helpers/DanaHelper.php
+++ src/Helpers/DanaHelper.php
@@ -199,8 +199,8 @@ class DanaHelper {
                     ],
                     [
                         'type' => 'NOTIFICATION',
-                        //'url'  => self::env_appApiUrl() .'/payment/dana/confirm'
-                        'url'  => 'https://webhook.site/5658b83b-f308-4e8e-844a-ca3ec53b2d59'
+                        'url'  => self::env_appApiUrl() .'/payment/dana/confirm'
+                        //'url'  => 'https://webhook.site/5658b83b-f308-4e8e-844a-ca3ec53b2d59'
                     ],
                 ]
             ]

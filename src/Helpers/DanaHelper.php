01f4ec44bdaf46f5f4b70bc4171869b09653250c
Budi <aku@budi.my.id>
2022-06-07 16:43:09 +0700

    1

--- src/Helpers/DanaHelper.php
+++ src/Helpers/DanaHelper.php
@@ -76,16 +76,16 @@ class DanaHelper {
 
         $data1 = self::danaCreateOrder($data);
 
-        $data1 = json_decode($data1);
+        //$data1 = json_decode($data1);
 
         $redirect_url = $data1['response']['body']['checkoutUrl'];
         $acquirementId = $data1['response']['body']['acquirementId'];
        
         $data2 = self::danaCreateSPI($data,$acquirementId);
 
-        //print_r($data1);
+        print_r($data1);
         //print_r($data2);
-        //exit();
+        exit();
         
         $response->authorization_id = $acquirementId;
         $response->bank_name = 'dana';

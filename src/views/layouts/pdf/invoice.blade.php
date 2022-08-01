c0f626476448c660996d38516c6a777ed77fb954
Budi <aku@budi.my.id>
2022-08-01 23:39:15 +0700

    Update invoice.blade.php

--- src/views/layouts/pdf/invoice.blade.php
+++ src/views/layouts/pdf/invoice.blade.php
@@ -215,12 +215,14 @@ footer {
        <tbody>
          <tr>
           <td style="background-color:#FFFFFF; text-align:left; padding-left:0px; line-height: 18px; font-size:14px; color:#777777">
-           <div style="margin-top:3px;"><strong style="font-size:18px;">PT VERTIKAL TRIP INDONESIA</strong></div>
-                        <div>Perum Guwosari Blok XII</div>
-                        <div>Jalan Abiyoso VII No.190</div>
-                        <div>Bantul Yogyakarta 55751 ID</div>
-                        <div>Phone : +62 857 4311-2112</div>
-                        <div>Email : guide@vertikaltrip.com</div>
+                        <div style="margin-top:3px;">
+                          <strong style="font-size:18px;">PT VERTIKAL TRIP INDONESIA</strong>
+                        </div>
+                        <div>PERUMNAS GUWOSARI BLOK XII</div>
+                        <div>JALAN ABIYOSO VII NO.190</div>
+                        <div>BANTUL D.I YOGYAKARTA 55751 INDONESIA</div>
+                        <div>PHONE : +62 857 4311-2112</div>
+                        <div>EMAIL : guide@vertikaltrip.com</div>
                   
            </td>
            <td style="background-color:#FFFFFF; text-align:right; padding-right:0px; line-height: 18px; font-size:14px; color:#777777">

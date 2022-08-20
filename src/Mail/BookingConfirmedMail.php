e5c808230438c278062fcacb9c006d0eba2fe6bd
Budi <aku@budi.my.id>
2022-08-20 10:32:18 +0700

    Update BookingConfirmedMail.php

--- src/Mail/BookingConfirmedMail.php
+++ src/Mail/BookingConfirmedMail.php
@@ -36,32 +36,11 @@ class BookingConfirmedMail extends Mailable
         
         $invoice = BookingHelper::create_invoice_pdf($shoppingcart);
 
+        /*
         $mail = $this->view('toursdk::layouts.mail.booking-confirmed')
                     ->text('toursdk::layouts.mail.booking-confirmed_plain')
                     ->subject('Booking Confirmation')
                     ->with('shoppingcart',$shoppingcart);
-                    
-                    //->attachData($invoice->output(), 'Invoice-'. $shoppingcart->confirmation_code .'.pdf', ['mime' => 'application/pdf']);
-
-        /*
-        if($shoppingcart->shoppingcart_payment->payment_type=="bank_transfer")
-        {
-            $instruction = BookingHelper::create_instruction_pdf($shoppingcart);
-            $mail->attachData($instruction->output(), 'Instruction-'. $shoppingcart->confirmation_code .'.pdf', ['mime' => 'application/pdf']);
-        }
-        */
-
-        /*
-        if($shoppingcart->shoppingcart_payment->payment_status!=4)
-        {
-            foreach($shoppingcart->shoppingcart_products()->get() as $shoppingcart_product)
-            {
-                
-                $ticket = BookingHelper::create_ticket_pdf($shoppingcart_product);
-                
-                $mail->attachData($ticket->output(), 'Ticket-'. $shoppingcart_product->product_confirmation_code .'.pdf', ['mime' => 'application/pdf']);
-            }
-        }
         */
 
     }

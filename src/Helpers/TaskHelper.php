b3e1ff9bdf3a5bec9a64f07f5cdaa0e30c1e4412
Budi <aku@budi.my.id>
2022-11-15 18:41:15 +0700

    Update TaskHelper.php

--- src/Helpers/TaskHelper.php
+++ src/Helpers/TaskHelper.php
@@ -7,21 +7,14 @@ use Google\Cloud\Tasks\V2\Task;
 
 class TaskHelper {
 
-	public function __construct() {
-		$this->task = new \stdClass();
-		$this->tw->projectId = env("TASK_PROJECT_ID");
-    	$this->tw->locationId = env("TASK_LOCATION_ID");
-    	$this->tw->queueId = env("TASK_QUEUE_ID");
-    	$this->tw->url = env("TASK_URL");
-	}
-
+	
 	public function create($payload)
 	{
 		$client = new CloudTasksClient();
-		$queueName = $client->queueName($this->tw->projectId, $this->tw->locationId, $this->tw->queueId);
+		$queueName = $client->queueName(env("TASK_PROJECT_ID"), env("TASK_LOCATION_ID"), env("TASK_QUEUE_ID"));
 
 		$httpRequest = new HttpRequest();
-		$httpRequest->setUrl($this->tw->url);
+		$httpRequest->setUrl(env("TASK_URL"));
 		$httpRequest->setHttpMethod(HttpMethod::POST);
 		if (isset($payload)) {
     		$httpRequest->setBody($payload);

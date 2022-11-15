a29cbdad3e8f16e7d9178a24ff98fd5a8c32f34b
Budi <aku@budi.my.id>
2022-11-15 19:16:23 +0700

    Update TaskController.php

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use budisteikul\toursdk\Helpers\WiseHelper;
-
+use budisteikul\toursdk\Helpers\LogHelper;
 
 class TaskController extends Controller
 {
 	public function task(Request $request)
     {
-    	
+    	LogHelper::log_webhook($request->getContent());
         $json = $request->getContent();
 		$data = json_decode($json);
         if($data->app=="wise")

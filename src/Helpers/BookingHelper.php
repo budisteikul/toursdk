14c688a32f82bbc7bdd2ed822504aaaa9fe216de
Budi <aku@budi.my.id>
2022-11-27 01:19:44 +0700

    Update BookingHelper.php

--- src/Helpers/BookingHelper.php
+++ src/Helpers/BookingHelper.php
@@ -2316,8 +2316,8 @@ class BookingHelper {
 		//{
 			//$value = number_format($value, 0, ',', ',');
 		//}
-		//return round($value);
-		return $value;
+		return round($value);
+		//return $value;
 	}
 	
 	public static function get_count($table="shoppingcart")

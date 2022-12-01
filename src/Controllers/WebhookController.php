5323cc2c8de2423f51caef6dd9829d32e6532767
Budi <aku@budi.my.id>
2022-12-01 15:27:12 +0700

    Update WebhookController.php

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -25,7 +25,95 @@ class WebhookController extends Controller
 
     public function test(Request $request)
     {
+            $confirmation_code = 'VT-221201005';
+            $session_id = 'f0e0038-b5f-4fdb-75d6-aa24ec5012a';
+
+            $shoppingcart = Shoppingcart::where('session_id',$session_id)->where('confirmation_code',$confirmation_code)->first();
+            if($shoppingcart)
+            {
+                
+                foreach($shoppingcart->shoppingcart_products as $product)
+                {
+                    $title = "New Booking: ". ProductHelper::datetotext($product->date) .' ('.$confirmation_code.')';
+                    
+
+                    $message = $product->title .'
+';
+                    $message .= ProductHelper::datetotext($product->date) .'
+';
+
+                    foreach($product->shoppingcart_product_details as $product_detail)
+                    {
+                        //Product
+                        if($product_detail->type=="product"|| $product_detail->type=="extra")
+                        {
+                            if($product_detail->unit_price == "Price per booking")
+                            {
+                                $message .= $product_detail->qty .' '. $product_detail->unit_price .' ('. $product_detail->people .' Person)
+';
+                            }
+                            else
+                            {
+                                $message .= $product_detail->qty .' '. $product_detail->unit_price .'
+';
+                            }
+                        }
+                        elseif($product_detail->type=="pickup")
+                        {
+                            $message .= $product_detail->title .'
+';
+                        }
+
+                        $message .= '
+';
+                        //Contact
+                        $main_contact = BookingHelper::get_answer_contact($shoppingcart);
+
+                        $message .= 'Name: '. $main_contact->firstName .' '. $main_contact->lastName .'
+';
+                        $message .= 'Phone: '. $main_contact->phoneNumber  .'
+';
+                        $message .= 'Email: '. $main_contact->email  .'
+';
+
+                        $message .= '
+';
+                        //Question
+                        foreach($shoppingcart->shoppingcart_questions()->where('when_to_ask','booking')->where('booking_id',$product->booking_id)->whereNotNull('label')->get() as $shoppingcart_question)
+                        {
+                                $message .= $shoppingcart_question->label .' : '. $shoppingcart_question->answer .'
+';
+                        }
+                        $participants = $shoppingcart->shoppingcart_questions()->where('when_to_ask','participant')->where('booking_id',$product->booking_id)->select('participant_number')->groupBy('participant_number')->get();
+                        foreach($participants as $participant)
+                        {
+                            $message .= 'Participant '. $participant->participant_number .'
+';
+                            foreach($shoppingcart->shoppingcart_questions()->where('when_to_ask','participant')->where('booking_id',$product->booking_id)->where('participant_number',$participant->participant_number)->get() as $participant_detail)
+                            {
+                                $message .= ''.$participant_detail->label .' : '. $participant_detail->answer .'
+';
+                            }
+                        }
         
+        
+                    }
+
+                    curl_setopt_array($ch = curl_init(), array(
+                    CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+                    CURLOPT_POSTFIELDS => array(
+                        "token" => env('PUSHOVER_TOKEN'),
+                        "user" => env('PUSHOVER_USER'),
+                        "title" => $title,
+                        "message" => $message,
+                    ),
+                    ));
+                    curl_exec($ch);
+                    curl_close($ch);
+                    
+                }
+                
+            }
     }
 	
     

e1026559f2b6455d1a3fb300d79c5d0d0ca2d5db
Budi <aku@budi.my.id>
2022-12-28 15:19:47 +0700

    Update TourSDKServiceProvider.php

--- src/TourSDKServiceProvider.php
+++ src/TourSDKServiceProvider.php
@@ -48,11 +48,7 @@ class TourSDKServiceProvider extends ServiceProvider
     protected function registerConfig()
     {
         
-        app()->config["filesystems.disks.local"] = [
-            'driver' => 'local',
-            'root' => env('APP_STORAGE', base_path() . '/storage'),
-            'throw' => false,
-        ];
+        
         
 
         app()->config["filesystems.disks.gcs"] = [

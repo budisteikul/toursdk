24093092b0a0c864c6502de0d1ee1586b06e4ce1
Budi <aku@budi.my.id>
2023-01-01 01:26:27 +0700

    Update APIController.php

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -819,8 +819,9 @@ class APIController extends Controller
                         $from = '<b>'.$channel_name.'</b>';
                     }
 
-                    $output = $user.$post_title.$rating.$title.$text.$from;
-                    
+                    //$output = $user.$post_title.$rating.$title.$text.$from;
+                    $output = $user.$post_title.$rating.$title.$text;
+
                     return '<div class="bd-callout bd-callout-theme shadow-sm rounded" style="margin-top:5px;margin-bottom:5px;" >'. $output .'</div>';
                 })
                 ->rawColumns(['style'])

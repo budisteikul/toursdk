f46ee658f549d2959d562ed268a7b94584781cb2
Budi <aku@budi.my.id>
2023-01-12 00:05:23 +0700

    Update ContentHelper.php

--- src/Helpers/ContentHelper.php
+++ src/Helpers/ContentHelper.php
@@ -550,7 +550,7 @@ class ContentHelper {
                 'localpayment_currency' => 'IDR',
                 'localpayment_total' => GeneralHelper::numberFormat(BookingHelper::convert_currency($shoppingcart->due_now,$shoppingcart->currency,'IDR'),'IDR'),
                 'localpayment_rate' => BookingHelper::text_rate($shoppingcart,'IDR'),
-                'localpayment_label' => '<strong class="mb-1">Local Payments</strong> <small>by Rapyd (Indonesia and Singapore)</small>',
+                'localpayment_label' => '<strong class="mb-1">Local Payments</strong> <small>(Indonesia and Singapore)</small>',
 
                 // IDR Currency
                 'idr_currency' => 'IDR',

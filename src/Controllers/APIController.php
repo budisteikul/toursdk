313f3bed31fda8eb49289c98102785b64ddac8f5
Budi <aku@budi.my.id>
2023-02-22 17:51:34 +0700

    Update APIController.php

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -1822,10 +1822,26 @@ class APIController extends Controller
                     var now = new Date().getTime();
                     var distance = countDownDate - now;
 
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-                    document.getElementById("payment_timer").innerHTML = minutes + " minutes " + seconds + " seconds ";
+                    if(days>0)
+                    {
+                        document.getElementById("payment_timer").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+                    }
+                    else if(hours>0)
+                    {
+                        document.getElementById("payment_timer").innerHTML = hours + "h "
+      + minutes + "m " + seconds + "s ";
+                    }
+                    else
+                    {
+                        document.getElementById("payment_timer").innerHTML = minutes + " minutes " + seconds + " seconds ";
+                    }
+                    
 
                     document.getElementById("timer_id").value = x[due_date];
                     if (distance < 0) {

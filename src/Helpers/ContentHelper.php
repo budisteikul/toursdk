0a4c3fdc4943e186de7d1df5df369274fb48f49f
Budi <aku@budi.my.id>
2023-07-22 12:54:44 +0700

    Update ContentHelper.php

--- src/Helpers/ContentHelper.php
+++ src/Helpers/ContentHelper.php
@@ -390,7 +390,7 @@ class ContentHelper {
 
                 // Local Payment Currency
                 
-                'localpayment_label' => '<strong class="mb-1">QR Code Payments</strong>',
+                'localpayment_label' => '<strong class="mb-1">Local Payments</strong>',
                 
                 /*
                 'localpayment_label' => '<strong class="mb-1">QR Payment</strong>

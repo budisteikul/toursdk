b9139a8c96b97b89d51bdbf922966f60a5247ea3
Budi <aku@budi.my.id>
2023-12-17 13:14:20 +0700

    Update TourSDKServiceProvider.php

--- src/TourSDKServiceProvider.php
+++ src/TourSDKServiceProvider.php
@@ -14,7 +14,7 @@ class TourSDKServiceProvider extends ServiceProvider
     public function register()
     {
         
-        $this->app['router']->aliasMiddleware('SettingMiddleware', \budisteikul\toursdk\Middleware\SettingMiddleware::class);
+        
     }
 
     /**
@@ -24,6 +24,7 @@ class TourSDKServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app['router']->aliasMiddleware('SettingMiddleware', \budisteikul\toursdk\Middleware\SettingMiddleware::class);
         $this->registerConfig();
         $this->loadViewsFrom(__DIR__.'/views', 'toursdk');
         $this->loadMigrationsFrom(__DIR__.'/migrations/2020_11_17_133006_create_categories_table.php');

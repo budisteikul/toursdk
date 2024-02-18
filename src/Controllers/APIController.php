15b73cdc81973b4ac61138fa03e6f08a1a700321
Budi <aku@budi.my.id>
2024-02-18 12:49:57 +0700

    Update APIController.php

--- src/Controllers/APIController.php
+++ src/Controllers/APIController.php
@@ -178,7 +178,7 @@ class APIController extends Controller
 
         $tourGuides[] = [
             'image' => config('site.assets').'/img/guide/budi.jpg',
-            'name' => 'Putu',
+            'name' => 'Budi',
             'description' => 'Tour Guide in Bali',
         ];
 

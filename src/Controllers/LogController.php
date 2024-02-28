db451b0bcce69cb59076a9e29e33b422b30e2366
Budi <aku@budi.my.id>
2024-02-29 01:05:59 +0700

    Update LogController.php

--- src/Controllers/LogController.php
+++ src/Controllers/LogController.php
@@ -15,14 +15,6 @@ class LogController extends Controller
     
     public function log($identifier="",Request $request)
     {
-        $value = $request->header('x-callback-token');
-        if(env('XENDIT_CALLBACK_TOKEN')!=$value)
-        {
-            return response()->json([
-                'message' => "ERROR"
-            ], 200);
-        }
-        
         $data1 = $request->getContent();
         LogHelper::log($data1,$identifier);
         return response('OK', 200)->header('Content-Type', 'text/plain');
